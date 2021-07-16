f21f48d5687c40a57cbe8f73b8d9ac5325646602
Aleksei <55848813+alekseisamuylov@users.noreply.github.com>
2021-07-16 09:56:47 +0300

    Remove default component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="home">
-    <h1>Here you can find any recipes! Just write orange, milk, coca-cola or any else and go to search.</h1>
+    <h1>
+      Here you can find any recipes! Just write orange, milk, coca-cola or any
+      else and go to search.
+    </h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
